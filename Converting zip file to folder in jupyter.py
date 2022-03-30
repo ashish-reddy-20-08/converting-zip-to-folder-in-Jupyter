b7570d232cbe1b8a961caef47c65bb2381e90035
M. Ashish Reddy <ashishreddy2222@gmail.com>
2022-03-30 10:34:49 +0530

    Update Converting zip file to folder in jupyter.py

--- Converting zip file to folder in jupyter.py
+++ Converting zip file to folder in jupyter.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
 
 
 import zipfile as zf
 files=zf.ZipFile("UrbanSound8K.zip",'r')
 
+# In the place of urbansound8k.zip write the zip file name that you uploaded in the jupyter 
+
+# In the place of urbansound8k in the below , kinldy write the name of the folder you want to keep after extraction 
+
 files.extractall("UrbanSound8K")
 files.close()
 
-
-# In[ ]:
 
 
 
